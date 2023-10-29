5fc80a1bcf1c6a28405e65fea261d291be7fdbae
El houcine  El yakoubi <elel-yak@e2r4p7.1337.ma>
2023-10-29 22:19:05 +0100

    ex01 in CPP00 - DONE

--- CPP-00/ex01/PhoneBook.cpp
+++ CPP-00/ex01/PhoneBook.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elel-yak <elel-yak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: elel-yak <elel-yak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 17:16:40 by elel-yak          #+#    #+#             */
-/*   Updated: 2023/10/28 13:59:06 by elel-yak         ###   ########.fr       */
+/*   Updated: 2023/10/29 22:15:25 by elel-yak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,19 @@ PhoneBook::PhoneBook()
 	number_of_contacts = 0;
 }
 
-void PhoneBook::set_indexContact(int Num_con){
+void PhoneBook::set_indexContact(int Num_con)
+{
 	number_of_contacts = Num_con;
 }
 
-int PhoneBook::get_indexContact(){
-	return(index_contact);
+int PhoneBook::get_indexContact()
+{
+	return (index_contact);
 }
 
-int PhoneBook::get_numberofContact(){
-	return(number_of_contacts);
+int PhoneBook::get_numberofContact()
+{
+	return (number_of_contacts);
 }
 
 void PhoneBook::add_contact(std::string temp[5])
@@ -78,30 +81,46 @@ void PhoneBook::show_details()
 		std::cout << "EoF" << std::endl;
 		exit(0);
 	}
-
 	std::string::size_type j = 0;
-	for (; std::isdigit(index_contact[j]) && j <  index_contact.size(); j++);
-	if (j != index_contact.size()) {
+	for (; std::isdigit(index_contact[j]) && j < index_contact.size(); j++)
+		;
+	if (j != index_contact.size())
 		std::cout << "Unvalid index" << std::endl;
-	} else {
-		const char* cstr = index_contact.c_str();
-		i = std::atoi(cstr);
+	else
+	{
+		if (index_contact.length() > 1 || std::stoi(index_contact) > 8 || std::stoi(index_contact) < 0)
+		{
+			std::cout << "Unvalid index" << std::endl;
+		}
+		else
+		{
+			const char *cstr = index_contact.c_str();
+			i = std::atoi(cstr);
 
-		if (i >= number_of_contacts) {
-			if (number_of_contacts > 0)
-				std::cout << "The index must be between 0 and " << number_of_contacts - 1 << std::endl;
+			if (i >= number_of_contacts)
+			{
+				if (number_of_contacts > 0)
+					std::cout << "The index must be between 0 and " << number_of_contacts - 1 << std::endl;
+				else
+					std::cout << "No contact has been found" << std::endl;
+			}
 			else
-				std::cout << "No contact has been found" << std::endl;
-		} else {
-			std::cout << std::left;
-			std::cout << "#-------------------------------------------#" << std::endl;
-			std::cout << "|-----------------Contact " << i << "-----------------|" << std::endl;
-			std::cout << std::setw(21) << "First name" << ": " << con[i].get_f_name() << std::endl;
-			std::cout << std::setw(21) << "Last name" << ": " << con[i].get_l_name() << std::endl;
-			std::cout << std::setw(21) << "Nick name" << ": " << con[i].get_n_name() << std::endl;
-			std::cout << std::setw(21) << "Darkest secret" << ": " << con[i].get_darkest_secret() << std::endl;
-			std::cout << std::setw(21) << "Phone number" << ": " << con[i].get_phone() << std::endl;
-			std::cout << "#-------------------------------------------#" << std::endl;
+			{
+				std::cout << std::left;
+				std::cout << "#-------------------------------------------#" << std::endl;
+				std::cout << "|-----------------Contact " << i << "-----------------|" << std::endl;
+				std::cout << std::setw(21) << "First name"
+						  << ": " << con[i].get_f_name() << std::endl;
+				std::cout << std::setw(21) << "Last name"
+						  << ": " << con[i].get_l_name() << std::endl;
+				std::cout << std::setw(21) << "Nick name"
+						  << ": " << con[i].get_n_name() << std::endl;
+				std::cout << std::setw(21) << "Darkest secret"
+						  << ": " << con[i].get_darkest_secret() << std::endl;
+				std::cout << std::setw(21) << "Phone number"
+						  << ": " << con[i].get_phone() << std::endl;
+				std::cout << "#-------------------------------------------#" << std::endl;
+			}
 		}
 	}
 }
